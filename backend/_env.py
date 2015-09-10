54a85876a5c0ac37ce0bcf5b6b2ea89976090f0e
Pegasus Wang <tianma201211@gmail.com>
2015-09-10 17:48:42 +0800

    env add utf-8 reload

--- backend/_env.py
+++ backend/_env.py
@@ -4,6 +4,10 @@
 import sys
 from os.path import abspath, dirname, join
 
+if sys.getdefaultencoding() != 'utf-8':
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
 PREFIX = abspath(
     join(
         dirname(abspath(__file__)), '../'
