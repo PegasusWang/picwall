24af4b465f9599245fc3364b3ddbfd67e1e20354
PegasusWang <291374108@qq.com>
2015-10-11 20:13:36 +0800

    modify

--- handlers/show.py
+++ handlers/show.py
@@ -69,7 +69,8 @@ class MobileShowHandler(BaseHandler):
     @addslash
     @coroutine
     def get(self, *args):
+        img_id = args[-1]
         img_url = 'http://ac-0pdchyat.clouddn.com/' + img_id.replace('_', '.')
-        img_url = img_url + '?imageMogr2/thumbnail/%sx/interlace/1' % width
-        print img_url
+        width = 540
+        img_url = img_url + '?imageMogr2/thumbnail/%dx/interlace/1' % width
         self.render("/mobile/site.html", src=img_url)
