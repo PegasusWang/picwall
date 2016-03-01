81da1536435eb9ffb36617b52cabbdf7a1bc5e7c
Pegasus Wang <tianma201211@gmail.com>
2016-03-01 18:01:54 +0800

    add mongoinstall shell for ubuntu14.04

---
+++ doc/mongodb_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ubuntu 14.04 install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
